24395c90e386645b7a9270356b0508249a424722
Sunehra Tabassum <sunehrat815@gmail.com>
2025-05-03 22:05:43 +0500

    Topics

---
+++ index.js
@@ -0,0 +1,12 @@
+// gonna build something cool heheh evil laugh
+
+const SIGMA_FACE = `
+( ͡° ͜ʖ ͡°)
+You’re running low on memory, stay Sigma.
+`;
+console.log(SIGMA_FACE);
+
+
+//Number systems__
+//Topics to Study
+// Number systems - types , image processing

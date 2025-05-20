465a4419ed6c477942fdfee9feb1d39ecba1fa5e
Sunehra Tabassum <sunehrat815@gmail.com>
2025-05-20 13:31:03 +0500

    CHECKING REPO

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-// gonna build something cool heheh evil laugh
+console.log("Hello, World!");
 
-const SIGMA_FACE = `
-( ͡° ͜ʖ ͡°)
-You’re running low on memory, stay Sigma.
-`;
-console.log(SIGMA_FACE);
-
-//Number systems__
-//Topics to Study
-// Number systems - types , image processing
-
-//Number System - binary & Decimal
-//polyart
-// two methods for making it ---frontend/canvas ....nodejs library .... i will explore both so yeah lets get started...

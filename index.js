f361947f629029af3225b0438a0d17f5e44050c5
Sunehra Tabassum <sunehrat815@gmail.com>
2025-05-20 14:33:19 +0500

    calculate idle time

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("Hello, World!");
+import { cpus } from "node:os";
+
+let totalcores = cpus().length ;
+let totalidletime = 0 ;
+let array = []
+cpus().forEach(CPU =>{
+ totalidletime += CPU.times.idle;
+ array.push(totalidletime)
+});
+
+const data = cpus().map((cpu, index) => ({
+    Core: index,
+    Idle: cpu.times.idle
+  }));
+
+ setInterval(() => {
+        console.table(array); 
+  }, 3000);
+
+
+
+
+
+
 

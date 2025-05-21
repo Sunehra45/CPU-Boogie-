dc563b87f47b18f235c0334164f22f33ce721de4
Sunehra Tabassum <sunehrat815@gmail.com>
2025-05-21 21:49:45 +0500

    h

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-// gonna build something cool heheh evil laugh
+import os from "node:os";
+console.log(os.machine());
+console.log(os.cpus().length)   //my system has 4 cores.   quard core
 
-const SIGMA_FACE = `
-( ͡° ͜ʖ ͡°)
-You’re running low on memory, stay Sigma.
-`;
-console.log(SIGMA_FACE);
 
-//Number systems__
-//Topics to Study
-// Number systems - types , image processing
-
-//Number System - binary & Decimal
-//polyart
-// two methods for making it ---frontend/canvas ....nodejs library .... i will explore both so yeah lets get started...

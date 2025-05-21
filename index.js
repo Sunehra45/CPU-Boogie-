16d070990331c0c6f193cd3420222be405e3185e
Sunehra Tabassum <sunehrat815@gmail.com>
2025-05-21 12:45:41 +0500

    Basic info

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-import { cpus } from "node:os";
+import os from "node:os";
 import chalk from "chalk";
+import { cpus } from "node:os";
 
-let totalcores = cpus().length ;
-let totalidletime = 0 ;
+function checkOS() {
+    const platform = os.platform();
+    switch (platform) {
+        case "win32":
+            return "Windows";
+        case "darwin":
+            return "MacOS";
+        case "linux":
+            return "Linux";
+        default:
+            return "Unknown OS";
+    }
+}
+
+let totalcores = cpus().length;
+let totalidletime = 0;
 let array = []
-
-cpus().forEach(CPU =>{
- totalidletime += CPU.times.idle;
+cpus().forEach(CPU => {
+    totalidletime += CPU.times.idle;
 });
-
-array.push(totalidletime); 
-
-console.log(chalk.cyan.bold("System Info:"))
-console.log( chalk.green(`Your system has ${totalcores} cores!`));
-console.log('The total system idle time is' +  " " + chalk.yellowBright(totalidletime));
-console.log( "Write"+ chalk.cyan(" .help ")+ "for more info");
+array.push(totalidletime);
 
 
-
-
-
-
-
+console.log(chalk.cyan.bold("System Info:"));
+console.log("Current system : " + chalk.yellowBright(checkOS()));
+console.log("Total CPU Cores : " + chalk.yellowBright(totalcores));
+console.log('Total system idle time is' + " " + chalk.yellowBright(totalidletime));
+console.log("Write" + chalk.cyan(" .help ") + "for more info");
 
 

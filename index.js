4e8ed29d78be051c0ff91f92fd1891efb659020e
Sunehra Tabassum <sunehrat815@gmail.com>
2025-05-22 09:52:36 +0500

    System

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import os, { cpus } from "node:os";
 import chalk from "chalk"; 
-import process from "node:process";
 
 function checkOS() {
     const platform = os.platform();
@@ -24,15 +23,36 @@ cpus().forEach(CPU => {
 });
 array.push(totalidletime);
 
-// free memory code:
-console.log(process.cpuUsage())
-// console.log(cpus()[0]);
-// console.log(chalk.cyan.bold("System Info:"));
-// console.log("Current system : " + chalk.yellowBright(checkOS()));
-// console.log("Total CPU Cores : " + chalk.yellowBright(totalcores));
-// console.log('Total system idle time is' + " " + chalk.yellowBright(totalidletime));
-// console.log("Write" + chalk.cyan(" .help ") + "for more info");
+let info = [];
+cpus().map((core,i)=>{                   //map returns array 
+   let detail  =  {
+  idletime: core.times.idle,
+  speed : core.speed
+ }
+ info.push(detail);
+});
+// console.table(info)
 
+setInterval(()=>{
+
+function totalUsage(){
+    const newCPUS = Object.values(cpus().times).reduce((a,b)=>{
+     return a+b ;});
+    //  console.log(newCPUS);
+}
+},1000)
+
+
+
+// console.log(cpus());
+
+console.log(chalk.cyan.bold("System Info:"));
+console.log("Current system : " + chalk.yellowBright(checkOS()));
+console.log("Total CPU Cores : " + chalk.yellowBright(totalcores));
+console.log('Host Name ' + os.hostname())
+console.log("Total Memory:" + os.totalmem()/(1024*1024*1024).toFixed(10)  + chalk.yellowBright("GB") );
+console.log('Total system idle time is' + " " + chalk.yellowBright(totalidletime));
+console.log("Write" + chalk.cyan(" .help ") + "for more info");
 
 
 
